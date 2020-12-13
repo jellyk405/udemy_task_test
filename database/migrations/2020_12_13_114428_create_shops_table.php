3b8b530ee5b35f5cc83b554cfbc14562f46e8bac
Tetsuya Yasuki <ktm.htm.rtm.405@hotmail.co.jp>
2020-12-13 13:30:07 +0900

    first commit

---
+++ database/migrations/2020_12_13_114428_create_shops_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->String('shop_name');
+            $table->unsignedBigInteger('area_id'); //FK
+            $table->timestamps();
+            $table->foreign('area_id')->references('id')->on('areas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
